b3c7cc8b8016d48a989c6af396c2be93562793b7
likuku-new-macbook <likuku.public@gmail.com>
2018-01-14 12:44:37 +0800

    use sqlalchemy create all Class_map_Tables

--- app/models.py
+++ app/models.py
@@ -1,20 +1,77 @@
 from app import app
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from app.database import Base
 import sqlite3
 
 class Setting(Base):
     __tablename__ = 'setting'
     id = Column(Integer, primary_key=True)
-    key = Column(String(50))
-    value = Column(String(255))
+    key = Column(String(50), nullable=False, unique=True)
+    value = Column(String)
 
     def __init__(self, key=None, value=None):
         self.key = key
         self.value = value
 
     def __repr__(self):
-        return '<Setting (key="%s", value="%s")>' % (self.key, self.value)
+        return '<Setting (key="%r", value="%r")>' % (self.key, self.value)
+
+class Project(Base):
+    __tablename__ = 'project'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False, unique=True)
+
+    def __init__(self, name=None):
+        self.name = name
+
+    def __repr__(self):
+        return '<Project (name="%r")>' % (self.name)
+
+class Contact(Base):
+    __tablename__ = 'contact'
+    id = Column(Integer, primary_key=True)
+    href = Column(String ,nullable=False)
+    class_name = Column(String(50) ,nullable=False)
+    label = Column(String(50) ,nullable=False)
+
+    def __init__(self, href=None, class_name=None, label=None):
+        self.href = href
+        self.class_name = value
+        self.label = label
+
+    def __repr__(self):
+        return '<Contact (href="%r", class_name="%r", label="%r")>' % (
+            self.href,self.class_name,self.label)
+
+class Photo(Base):
+    __tablename__ = 'photo'
+    id = Column(Integer, primary_key=True)
+    href = Column(String, nullable=False, unique=True)
+    width = Column(Integer, nullable=False)
+    src = Column(String, nullable=False, unique=True)
+
+    def __init__(self, href=None, width=None, src=None):
+        self.href = href
+        self.width = value
+        self.src = src
+
+    def __repr__(self):
+        return '<Photo (href="%r", width="%r", src="%r")>' % (
+            self.href,self.width,self.src)
+
+
+class Projects_Photos(Base):
+    __tablename__ = 'projects_photos'
+    project_id = Column(Integer, ForeignKey('project.id'), primary_key=True)
+    photo_id = Column(Integer, ForeignKey('photo.id'), primary_key=True)
+
+    def __init__(self, project_id=None, photo_id=None):
+        self.project_id = project_id
+        self.photo_id = photo_id
+
+    def __repr__(self):
+        return '<Projects_Photos (project_id="%r", photo_id="%r")>' % (
+            self.project_id,self.photo_id)
 
 #####
 def connect_db():
