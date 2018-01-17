540bda728505c90bec7ffb52c85be58ec62a5c3d
likuku-new-macbook <likuku.public@gmail.com>
2018-01-17 16:34:33 +0800

    update project_root/config.py

--- config.py
+++ config.py
@@ -4,3 +4,7 @@ import os
 PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
 DATABASE = os.path.join(PROJECT_ROOT, 'instance', 'data.db')
 DEBUG = False
+
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(PROJECT_ROOT,'instance','data.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
